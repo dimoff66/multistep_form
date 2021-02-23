c38e9455a2ccc83fd38148a85ad45576d03eb323
Dmitry Reutov <32884656+dimoff66@users.noreply.github.com>
2021-02-23 13:18:10 +0300

    Update Specialization.js

--- src/components/stepForm/Specialization.js
+++ src/components/stepForm/Specialization.js
@@ -4,7 +4,7 @@ import mas_reg from './mas_reg.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Map_spec } from './Map_spec';
 
-export const Specialization = ({ formData, setForm, navigation}) => {
+export const Specialization = ({ formData, setForm, navigation, specializations, toggleSpecialization }) => {
 
    
     const { firstName, lastName, nickName } = formData;
@@ -17,25 +17,23 @@ export const Specialization = ({ formData, setForm, navigation}) => {
              <h1 className="main_h1">Услуги и специальность {isChecked ? "True":"False"}</h1>
              <p className="top_p">Выберите одну или несколько специальностей</p>
              <div className="wraper">
-             {Map_spec.map((checkbox, key) => (
-             
-             
-             <>
-             <input type="checkbox" class="custom-checkbox" 
-             id={checkbox.label} 
-             name={checkbox.label}
-             checked={isChecked}
-             onChange={(e)=>{setIsChecked(e.target.checked.name)}}
-             
-             key={checkbox.label}
-            
-              
-              />
-             <label
-             htmlFor={checkbox.label}><span style={{textAlign:'left', width:'50%'}}>{checkbox.label}</span><div className="lined"></div></label>
-             </>
+             {Map_spec.map((checkbox, key) => {
+               
+               return (
+                <>
+                <input type="checkbox" class="custom-checkbox" 
+                  id={checkbox.label} 
+                  name={checkbox.label}
+                  checked={specializations.includes(checkbox.key)}
+                  onChange={() => toggleSpecialization(checkbox.key)}
+                  
+                  key={checkbox.label}
+                />
+                <label
+                htmlFor={checkbox.label}><span style={{textAlign:'left', width:'50%'}}>{checkbox.label}</span><div className="lined"></div></label>
+                </>
          
-            ))}
+             )})}
             </div>
             <button 
              className="dale_2"
